00efee69333658a8b0ab12083c8505938e3ca96c
mike <mike@bettercallsaul.com>
2017-07-09 01:58:16 +0800

    [SimCal.evalSimple, 200000001 chars] java.lang.OutOfMemoryError: Java heap space

--- sirap-basic/src/main/java/com/sirap/basic/math/SimCal.java
+++ sirap-basic/src/main/java/com/sirap/basic/math/SimCal.java
@@ -1,11 +1,8 @@
 package com.sirap.basic.math;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Stack;
 import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import com.sirap.basic.exception.MexException;
 import com.sirap.basic.util.EmptyUtil;
@@ -22,26 +19,11 @@ public class SimCal {
 	public static String evaluate(String content, int scale) {
 		String temp = preProcess(content);
 		
-		double tempResult = evaluateInner(temp);
+		double tempResult = evalSimple(temp);
 		BigDecimal bd = MathUtil.divide(tempResult, 1, scale);
 		String result = StrUtil.removePointZeroes(bd.toPlainString());
 
 		return result;
-	}
-	
-	private static double evaluateInner(String content) {
-		String temp = content;
-		String regex = "\\(([^\\(]*?)\\)";
-		Matcher ma = StrUtil.createMatcher(regex, temp);
-		if(ma.find()) {
-			String expression = ma.group(1);
-			double tempResult = evalSimple(expression);
-			temp = temp.replace(ma.group(), cover(tempResult + ""));
-			return evaluateInner(temp);
-		} else {
-			double value = evalSimple(temp);
-			return value;
-		}
 	}
 	
 	private static String cover(String value) {
@@ -64,50 +46,16 @@ public class SimCal {
 	
 	private static String preProcess(String expression) {
 		String content = expression.replaceAll("\\s+", "");
-		content = content.replaceAll("^0+", "0");
 		content = StrUtil.removePointZeroes(content);
 		char start = content.charAt(0);
 		if(start == '-') {
 			content = "_" + content.substring(1);
 		}
-		
-		if(!isMatchedParenthesis(content)) {
-			String msg = "Contains unmatched parenthesis";
-			throw new MexException(msg);
-		}
-		
-		String regexStartsWith = "^[+\\-x\\*/\\)]";
-		Matcher ma = StrUtil.createMatcher(regexStartsWith, content);
-		if(ma.find()) {
-			String msg = "Expression can't start with [" + ma.group() + "]";
-			throw new MexException(msg);
-		}
 
 		String regexAtLeastOneOperator = "[+\\-x\\*/]";
-		ma = StrUtil.createMatcher(regexAtLeastOneOperator, content);
+		Matcher ma = StrUtil.createMatcher(regexAtLeastOneOperator, content);
 		if(!ma.find()) {
-			String msg = "Expression contains no operator";
-			throw new MexException(msg);
-		}
-		
-		String regexEndsWith = "[+\\-x\\*/\\(]$";
-		ma = StrUtil.createMatcher(regexEndsWith, content);
-		if(ma.find()) {
-			String msg = "Expression can't end with [" + ma.group() + "]";
-			throw new MexException(msg);
-		}
-
-		String regexConnectedOps = "[^+\\-x/\\*\\(]\\(";
-		ma = StrUtil.createMatcher(regexConnectedOps, content);
-		if(ma.find()) {
-			String msg = "There should be an operator between " + ma.group();
-			throw new MexException(msg);
-		}
-		
-		regexConnectedOps = "\\)[^+\\-x/\\*\\)]";
-		ma = StrUtil.createMatcher(regexConnectedOps, content);
-		if(ma.find()) {
-			String msg = "There should be an operator between " + ma.group();
+			String msg = "Expression contains no operator.";
 			throw new MexException(msg);
 		}
 
@@ -115,63 +63,117 @@ public class SimCal {
 	}
 	
 	public static double evalSimple(String expression) {
-		String content = cover(expression);
-		List<Calp> items = fetchAllItems(content);
-		if(items.isEmpty()) {
-			throw new MexException("No operands at all, illegal [" + content + "]");
+		String temp = cover(expression);
+		boolean hasPreviousBeenSet = false;
+		boolean hasAceBeenSet = false;
+		
+		Calp previous = null;
+		Calp ace = null;
+		Calp bax = null;
+		
+		StringBuffer what = new StringBuffer();
+		String operators = "+-*xX/";
+
+		boolean nextShouldBeOperator = false;
+		for(int i = 0; i < temp.length(); i++) {
+			char ch = temp.charAt(i);
+			boolean isOperator = operators.indexOf(ch) >= 0;
+			if(nextShouldBeOperator && !isOperator) {
+				String msg = "There should be an operator before {0}, index of {1}";
+				throw new MexException(StrUtil.occupy(msg, ch, i));
+			}
+			if(isOperator) {
+				if(nextShouldBeOperator) {
+					nextShouldBeOperator = false;
+				}
+				String number = what.toString();
+				if(EmptyUtil.isNullOrEmpty(number)) {
+					String msg = "There should be a number between {0} and {1}.";
+					throw new MexException(StrUtil.occupy(msg, "SHIT", ch));
+				}
+				what.setLength(0);
+				Calp item = createCalp(number, ch);
+				if(!hasPreviousBeenSet) {
+					hasPreviousBeenSet = true;
+					previous = item;
+					continue;
+				}
+				if(!hasAceBeenSet) {
+					hasAceBeenSet = true;
+					ace = item;
+					continue;
+				}
+				bax = item;
+				if(ace.isHeavierThan(previous)) {
+					double tempLate = late(ace.number, ace.operator, bax.number);
+					ace.number = tempLate;
+					ace.operator = bax.operator;
+				} else {
+					double tempLate = late(previous.number, previous.operator, ace.number);
+					previous.number = tempLate;
+					previous.operator = ace.operator;
+					ace = bax;
+				}
+			} else {
+				if(ch == '(') {
+					if(what.length() > 0) {
+						String msg = "There should be an operator between {0} and left parenthesis {1}.";
+						throw new MexException(StrUtil.occupy(msg, what, ch));
+					}
+					int nextMatched = indexOfNextMatchedParenthesis(temp, i);
+					if(nextMatched < 0) {
+						throw new MexException("There exists unmatched parenthesis for the (, index of " + i);
+					}
+					String subString = temp.substring(i + 1, nextMatched);
+					double subResult = evalSimple(subString);
+					what.append(subResult);
+					nextShouldBeOperator = true;
+					i = nextMatched;
+				} else {
+					what.append(ch);
+				}
+			}
 		}
 		
-		Calp previous = items.get(0);
-		if(items.size() == 1) {
-			return previous.number;
-		}
-		Calp ace = items.get(1);
-		for(int i = 2; i < items.size(); i++) {
-			Calp bax = items.get(i);
-
+		String number = what.toString();
+		if(number.isEmpty()) {
+			String msg = "The expression {0} shouldn't end with an operator.";
+			throw new MexException(StrUtil.occupy(msg, expression));
+		} else {
+			Calp item = createCalp(number, '$');
+			if(!hasPreviousBeenSet) {
+				return item.number;
+			}
+			if(!hasAceBeenSet) {
+				ace = item;
+				double tempFinal = late(previous.number, previous.operator, ace.number);
+				return tempFinal;
+			}
+			bax = item;
 			if(ace.isHeavierThan(previous)) {
-				double temp = late(ace.number, ace.operator, bax.number);
-				ace.number = temp;
+				double tempLate = late(ace.number, ace.operator, bax.number);
+				ace.number = tempLate;
 				ace.operator = bax.operator;
 			} else {
-				double temp = late(previous.number, previous.operator, ace.number);
-				previous.number = temp;
+				double tempLate = late(previous.number, previous.operator, ace.number);
+				previous.number = tempLate;
 				previous.operator = ace.operator;
 				ace = bax;
 			}
+			
+			double tempFinal = late(previous.number, previous.operator, ace.number);
+			return tempFinal;
 		}
-		
-		double temp = late(previous.number, previous.operator, ace.number);
-		return temp;
 	}
 	
-	public static List<Calp> fetchAllItems(String expression) {
-		XXXUtil.nullOrEmptyCheck(expression, "expression");
-		String temp = expression;
-		String regexOperandAndOperator = "^([^+\\-x\\*/]+)([+\\-x\\*/]|)";
-		Pattern pa = Pattern.compile(regexOperandAndOperator, Pattern.CASE_INSENSITIVE);
-		Matcher ma = pa.matcher(temp);
-		List<Calp> items = new ArrayList<>();
-		while(ma.find()) {
-			String op = ma.group(2);
-			Calp item = new Calp();
-			String value = uncover(ma.group(1));
-			item.number = toDouble(value);
-			if(!EmptyUtil.isNullOrEmpty(op)) {
-				item.operator = op.charAt(0);
-			}
-			items.add(item);
-			int endIndex = ma.end(2);
-			temp = temp.substring(endIndex);
-			ma = pa.matcher(temp);
-		}
-
-		if(!temp.isEmpty()) {
-			String msg = "There should be no operator at the beginning of " + temp;
-			throw new MexException(msg);
-		}
+	private static Calp createCalp(String what, char op) {
+		Calp item = new Calp();
+		String value = uncover(what.toString());
+		item.origin = value;
+		item.number = toDouble(value);
+		item.operator = op;
 		
-		return items;
+		return item;
 	}
 	
 	private static double late(double a, char operator, double b) {
@@ -196,36 +198,41 @@ public class SimCal {
 		try {
 			return Double.parseDouble(src);
 		} catch (Exception ex) {
-			throw new MexException("Unable to parse double " + src);
+			String msg = "Unable to parse {0} as double.";
+			throw new MexException(StrUtil.occupy(msg, src));
 		}
 	}
 	
-	public static boolean isMatchedParenthesis(String source) {
+	private static int indexOfNextMatchedParenthesis(String source, int indexOfStartParenthesis) {
 		Stack<String> de = new Stack<>();
 		int len = source.length();
-		for(int i = 0; i < len; i++) {
+		for(int i = indexOfStartParenthesis; i < len; i++) {
 			char ch = source.charAt(i);
 			if(ch == '(') {
 				de.push(ch + "");
 			} else if(ch == ')') {
 				if(de.isEmpty()) {
-					return false;
+					throw new MexException("There exists unmatched parenthesis for the ), index of " + i);
 				} else {
 					de.pop();
+					if(de.isEmpty()) {
+						return i;
+					}
 				}
 			}
 		}
 		
-		return de.isEmpty();
+		return -1;
 	}
 }
 
 class Calp {
+	public String origin;
 	public double number;
 	public char operator = '$';
 	
 	public String toString() {
-		String temp = number + ", " + operator;
+		String temp = origin + ", " + operator;
 		return temp;
 	}
 	
